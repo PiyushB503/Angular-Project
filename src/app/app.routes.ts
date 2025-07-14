c7a283fc83afb953782ec79158c2dc594602a34f
Piyush Bhardwaj <piyush.bhardwaj1@sourcefuse.com>
2025-07-14 11:14:53 +0530

    changes in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BookFormComponent } from './components/book-form/book-form.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      { path: '', redirectTo: 'add', pathMatch: 'full' },
+  { path: 'add', component: BookFormComponent },
+   { path: 'edit/:isbn', component: BookFormComponent },
+  { path: 'list', component: BookListComponent }
+];
